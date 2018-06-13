427e988956ba4bb1042b9069a59e63dbdc3eebbb
CybertekSchool <github@cybertekschool.com>
2018-06-13 18:54:17 -0400

    automation-project

---
+++ src/test/java/com/cybertek/Batch8.java
@@ -0,0 +1,20 @@
+package com.cybertek;
+
+import org.openqa.selenium.WebDriver;
+
+import com.github.javafaker.Faker;
+
+public class Batch8 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Just learning maven");
+		Faker faker = new Faker();
+		String creditCard = faker.finance().creditCard();
+		System.out.println(creditCard);
+		
+		
+		WebDriver driver;
+	}
+
+}
